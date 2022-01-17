079ef7655301121fa4fc89e8a1eb982592c0665f
ChriEffe <christianferrazzano93@gmail.com>
2022-01-17 23:50:52 +0100

    fix

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+<header>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png" alt="">
+</header>
+</template>
+
+<script>
+export default {
+    name : "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+header  {
+    height: 50px;
+    background-color: #2e3a46;
+    img {
+        height: 100%;
+        padding: 0.5em;
+    }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,85 @@
+<template>
+<div class="container-fluid bg">
+    <div class="container-70">
+        <div class="row row-cols-6 pt-5 pb-5" v-if="cards">
+            <div class="col py-4 g-3 mx-3 flex" v-for="(card, index) in cards" :key="index">
+                <img :src="card.poster" alt="">
+                <h2>{{ card.title }}</h2>
+                <h3>{{ card.author }}</h3>
+                <h4>{{ card.year }}</h4>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+name: 'Main',
+data(){
+    return{
+        cardsApi: 'https://flynn.boolean.careers/exercises/api/array/music',
+        cards: null,
+    };
+},
+created() {
+        axios
+        .get(this.cardsApi)
+        .then((result) => {
+        console.log(result.data.response);
+        this.cards = result.data.response;
+        })
+        .catch((error) => {
+        console.log(error);
+        })
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+*{
+    border: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+.bg{
+    background-color: #1e2d3b;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    .container-70{
+        width: 70%;
+        margin: 0 auto;
+        .flex{
+            display: flex;
+            flex-direction: column;
+            justify-content: start;
+            align-items: center;
+        }
+        .col{
+            background-color: #2e3a46;
+            img{
+            width: 100%;
+            }
+        }
+    }
+}
+h1{
+    color: white;
+    text-transform: uppercase;
+    text-align: center;
+}
+h2{
+    font-size: 1.2em;
+    color: white;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 0.5em;
+}
+h3, h4{
+    color: #808080;
+    text-align: center;
+    font-size: 0.7em;
+}
+</style>
